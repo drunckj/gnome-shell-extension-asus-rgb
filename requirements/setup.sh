053512fbd19251b2b9814186967ab4db22de7360
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2020-08-17 19:55:50 +0530

    Update setup.sh

--- requirements/setup.sh
+++ requirements/setup.sh
@@ -2,6 +2,7 @@
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 sudo apt install libusb-1.0-0 libusb-1.0-0-dev 
 sudo apt install autoconf
+git clone https://github.com/wroberts/rogauracore.git
 cd rogauracore/
 autoreconf -i
 ./configure
